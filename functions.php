fb36d0219edbeb39bf094bed16c82e6c076965d1
Vitnere <nemanjakolar@gmail.com>
2016-07-15 15:47:13 +0200

    Main Post Loop

--- functions.php
+++ functions.php
@@ -7,20 +7,31 @@
  */
 ?>
 
-<?php
-//Register Nav Walker class_alias
+    <?php
+    //Register Nav Walker class_alias
 
-require_once('wp_bootstrap_navwalker.php');
+    require_once('wp_bootstrap_navwalker.php');
 
-//Theme Support
-function wpb_theme_setup()
-{
-    //nav Menus
-    register_nav_menus(array(
-        'primary'=> __('Primary Menu')
-    ));
-}
+    //Theme Support
+    function wpb_theme_setup()
+    {
+        add_theme_support('post-thumbnails');
 
-    add_action('after_setup_theme','wpb_theme_setup');
+        //nav Menus
+        register_nav_menus(array(
+            'primary'=> __('Primary Menu')
+        ));
+    }
 
-?>
+    add_action('after_setup_theme','wpb_theme_setup');//to add something
+
+    //Excerpt Length Control
+    function set_excerpt_length()
+    {
+        return 40;//post length in words
+    }
+
+    add_filter('excerpt_length','set_excerpt_length');//to edit something
+
+
+
