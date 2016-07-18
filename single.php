88e478ef13e5195571a08d9ba529f70c12223c17
Vitnere <nemanjakolar@gmail.com>
2016-07-18 16:04:06 +0200

    template init (get_template_part())

--- single.php
+++ single.php
@@ -6,51 +6,16 @@
  * Time: 2:42 PM
  */
 ?>
-
 <?php get_header(); ?>
-
 <div class="container">
-
-
     <div class="row">
         <div class="col-sm-8 blog-main">
-
             <?php if(have_posts()) : ?>
                 <?php while(have_posts()) : the_post(); ?>
-                    <div class="blog-post">
-                        <h2 class="blog-post-title">
-                                <?php the_title(); ?>
-                            </a>
-                        </h2>
-                        <p class="blog-post-meta">
-                            <?php the_date('F j,Y g:i a'); ?>
-                            by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-                                <?php the_author(); ?>
-                            </a>
-                        </p>
-
-                        <?php if(has_post_thumbnail()) :?>
-                            <div class="post-thumb">
-                                <?php the_post_thumbnail(); ?>
-                            </div>
-                        <?php endif; ?>
-                        <?php the_content(); ?>
-                        <hr>
-                        <?php comments_template();?>
-
-                    </div><!-- /.blog-post -->
+                    <?php get_template_part('content'); ?>
                 <?php endwhile; ?>
             <?php else :?>
                 <?php  __('There is no posts for view')?>
             <?php endif; ?>
-
-            <nav>
-                <ul class="pager">
-                    <li><a href="#">Previous</a></li>
-                    <li><a href="#">Next</a></li>
-                </ul>
-            </nav>
-
         </div><!-- /.blog-main -->
-
 <?php get_footer(); ?>
